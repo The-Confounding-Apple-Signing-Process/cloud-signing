a0663f9d27c18cce8af7932ea0c715a548e48449
Uday Pandey <uday.pandey@gmail.com>
2021-11-05 10:40:38 +0000

    Added sample project

---
+++ CloudSigning/CloudSigningApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudSigningApp.swift
+//  CloudSigning
+//
+//  Created by Uday Pandey on 05/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct CloudSigningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
